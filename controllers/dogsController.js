1e31e893c48b83e5ae74e495f4076bcd6c5758ae
James Sinkler <jamessinkler713@gmail.com>
2020-04-26 10:24:41 -0700

    updates rating the dog route

--- controllers/dogsController.js
+++ controllers/dogsController.js
@@ -22,6 +22,19 @@ router.get('/new', (req, res) => {
   res.render('new');
 })
 
+// DELETE Dogs Destroy
+router.delete('/:id', (req, res) => {
+  db.Dog.findByIdAndDelete(req.params.id, (err, deletedDog) => {
+    if (err) {
+      return res.send(err);
+    }
+
+    console.log('Deleted Dog = ', deletedDog);
+
+    res.redirect('/dogs');
+  });
+});
+
 router.post('/create', multipartMiddleware,  (req, res)=> {
   let body = (req.body);
   cloudinary.uploader.upload(req.files.image.path, function(result) {
@@ -53,5 +66,35 @@ router.get('/:id', async (req, res) => {
       res.send(err);
     }
 });
+
+
+
+// PUT Dogs Update
+router.put('/:id', async (req, res) => {
+  try{
+
+    // ADDED Section for Ratings
+    //console.log(req.body)
+    //console.log(req.body.ratings)
+    if (!req.body.ratings) { // new check for what kind of update
+    const updatedDog = await db.Dog.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {new: true}
+    )
+    } else { // This is the new case to push another rating
+      const updateDog = await db.Dog.findByIdAndUpdate(
+        req.params.id,
+        {$push: {ratings: req.body.ratings}},
+        {new: true}
+      )
+    }
+    // changed to put back to the dogs show page
+    res.redirect(`/dogs/${req.params.id}`);
+  } catch (err) {
+    return res.send(err);
+  }
+});
+
 
 module.exports = router;
